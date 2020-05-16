0f77301e1df7d52e237af98daa3f0db3be8476f5
Alan Leverenz <awleverenz@aol.com>
2020-05-15 23:19:36 -0400

    setup Navbar and Users components

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export class Navbar extends Component {
+  // set default in case none provided
+  static defaultProps = {
+    title: 'Github Finder',
+    icon: 'fab fa-github',
+  };
+  // static constructor
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired,
+  };
+
   render() {
     return (
       <nav className='navbar bg-primary'>
         <h1>
-          <i class='fab fa-github'></i>
-          Navbar
+          <i className={this.props.icon}></i> {this.props.title}
         </h1>
       </nav>
     );
   }
 }
-
+// only need export statement once
 export default Navbar;
